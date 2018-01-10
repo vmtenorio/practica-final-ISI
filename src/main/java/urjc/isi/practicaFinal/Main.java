f7c3d13caa6e5b9df0e3809c16248da42ea66801
rosu123 <rosu.perela@gmail.com>
2018-01-10 19:18:44 +0100

    añadido soporte para PostgreSQL

--- src/main/java/urjc/isi/practicaFinal/Main.java
+++ src/main/java/urjc/isi/practicaFinal/Main.java
@@ -5,6 +5,13 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
 import java.util.StringTokenizer;
 
 import javax.servlet.MultipartConfigElement;
@@ -12,6 +19,9 @@ import javax.servlet.MultipartConfigElement;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
+import java.net.URISyntaxException;
+import java.net.URI;
 
 // This code is quite dirty. Use it just as a hello world example 
 // to learn how to use JDBC and SparkJava to upload a file, store 
@@ -23,10 +33,82 @@ public class Main {
         return("Unread Code");
     };
 	
+    
+    
+    
+    //////////////////////////////////////////////////////////////////////////////
+    
+    private static Connection connection;
+    
+    // Used to illustrate how to route requests to methods instead of
+    // using lambda expressions
+    public static String doSelect(Request request, Response response) {
+	return select (connection, request.params(":table"), 
+		       request.params(":film"));
+    }
+
+    public static String select(Connection conn, String table, String film) {
+	String sql = "SELECT * FROM " + table + " WHERE film=?";
+
+	String result = new String();
+	
+	try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+		pstmt.setString(1, film);
+		ResultSet rs = pstmt.executeQuery();
+                // Commit after query is executed
+		connection.commit();
+
+		while (rs.next()) {
+		    // read the result set
+		    result += "film = " + rs.getString("film") + "\n";
+		    System.out.println("film = "+rs.getString("film") + "\n");
+
+		    result += "actor = " + rs.getString("actor") + "\n";
+		    System.out.println("actor = "+rs.getString("actor")+"\n");
+		}
+	    } catch (SQLException e) {
+	    System.out.println(e.getMessage());
+	}
+	return result;
+    }
+    
+    
+    public static void insert(Connection conn, String film, String actor) {
+	String sql = "INSERT INTO films(film, actor) VALUES(?,?)";
+
+	try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+		pstmt.setString(1, film);
+		pstmt.setString(2, actor);
+		pstmt.executeUpdate();
+	    } catch (SQLException e) {
+	    System.out.println(e.getMessage());
+	}
+    }
+    
+    
+    //////////////////////////////////////////////////////////////////////////////
+    
+    
+    
+    
+    
 	public static void main(String[] args) throws 
-	ClassNotFoundException {
+	ClassNotFoundException, SQLException, URISyntaxException {
     	
     port(getHerokuAssignedPort());
+    
+    // This code only works for PostgreSQL in Heroku
+ 	// Connect to PostgreSQL in Heroku
+ 	URI dbUri = new URI(System.getenv("DATABASE_URL"));
+ 	String username = dbUri.getUserInfo().split(":")[0];
+ 	String password = dbUri.getUserInfo().split(":")[1];
+ 	String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+ 	connection = DriverManager.getConnection(dbUrl, username, password);
+ 	
+ 	// PostgreSQL default is to auto-commit (1 transaction / statement execution)
+         // Set it to false to improve performance
+ 	connection.setAutoCommit(false);
+    
     
     //connection = DriverManager.getConnection("jdbc:sqlite:sample_graph.db");
     //connection.setAutoCommit(false);
@@ -46,7 +128,16 @@ public class Main {
  		String result = "File uploaded!";
  		try (InputStream input = req.raw().getPart("uploaded_films_file").getInputStream()) { 
  			// getPart needs to use the same name "uploaded_films_file" used in the form
+ 			
+ 			
+ 			////
+ 			// Prepare SQL to create table
+ 			Statement statement = connection.createStatement();
 
+ 			// This code only works for PostgreSQL
+ 			statement.executeUpdate("drop table if exists films");
+ 			statement.executeUpdate("create table films (film text, actor text)");
+ 			/////
  			
  			// Read contents of input stream that holds the uploaded file
  			InputStreamReader isr = new InputStreamReader(input);
@@ -66,7 +157,16 @@ public class Main {
 			    // Now get actors and insert them
 			    while (tokenizer.hasMoreTokens()) {
 			    	graph.addEdge(film, tokenizer.nextToken());
+			    	////
+			    	insert(connection, film, tokenizer.nextToken());
+			    	////
 			    }
+			    ////
+			    // Commit only once, after all the inserts are done
+			    // If done after each statement performance degrades
+			    connection.commit();
+			    ////
+			    
 			}
 
  	        // print out graph
