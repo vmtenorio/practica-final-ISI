01be327153dd72f496e08aac7ec5a76d5025454c
Victor Manuel Tenorio Gomez <vm.tenorio@alumnos.urjc.es>
2018-01-17 12:21:27 +0100

    Test objeto que no esta en el grafo

--- src/test/java/urjc/isi/practicaFinal/PathFinderTest.java
+++ src/test/java/urjc/isi/practicaFinal/PathFinderTest.java
@@ -13,13 +13,20 @@ public class PathFinderTest {
 	
 	@Before
 	public void setUp() {
-		g = new Graph("routes.txt", " ");
-		pf = new PathFinder(g, "ORD");
+		g = new Graph("resources/routes.txt", " ");
+		pf = new PathFinder(g, "ATL");
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testHappyPath() {
+		assertEquals("Calcula mal la distancia", pf.distanceTo("PHX"), 2);
+		assertEquals("Calcula mal el camino", pf.pathTo("PHX").toString(), "ATL ORD PHX ");
+	}
+	
+	@Test
+	public void testObjetoNoEncontrado() {
+		assertEquals("Distancia a un objeto que no esta en el grafo", pf.distanceTo("ASDF"), Integer.MAX_VALUE);
+		assertEquals("Camino a un obejeto que no esta en el grafo", pf.pathTo("ASDF").toString(), "");
 	}
 
 }
