63a0b7eabb1ea2e59a17987f0bd0582978e3f0e1
cgomezcasa <cayegomez1995@gmail.com>
2018-01-22 10:02:02 +0100

    Test query actor: lista vacía corregido

--- src/test/java/urjc/isi/practicaFinal/QueriesTest.java
+++ src/test/java/urjc/isi/practicaFinal/QueriesTest.java
@@ -1,15 +1,22 @@
 package urjc.isi.practicaFinal;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class QueriesTest {
 	
+	private static Queries query;
 	Graph g;
 	String actor;
 	
+	@Before
+	public void setUp() {
+		query = new Queries();
+	}
+	
 	@Test (expected=IllegalArgumentException.class)
 	public void testObjetoNoEncontrado () {
-		g.adjacentTo(actor);
+		query.actorQuery(g, actor);
 	}
 
 }
