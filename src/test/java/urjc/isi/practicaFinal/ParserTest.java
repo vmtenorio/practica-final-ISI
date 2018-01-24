31525a7ddb7a38a830df418885b62be5ef1a705d
cgomezcasa <cayegomez1995@gmail.com>
2018-01-24 18:12:30 +0100

    añadidos test a ParserTest

--- src/test/java/urjc/isi/practicaFinal/ParserTest.java
+++ src/test/java/urjc/isi/practicaFinal/ParserTest.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 public class ParserTest {
 	
 	
+	// GRAFO getFilmTitle
 	// Camino [1, 2, 3]
 	@Test (expected=NullPointerException.class)
-	public void testNullParam() {
+	public void testNullTitle() {
 		String film = null;
 		Parser.getFilmTitle(film);
 	}
-	
+
 	// Camino [1, 2, 4, 5]
 	@Test (expected=IllegalArgumentException.class)
-	public void testParamIlegalArgument() {
+	public void testTitleIlegalArgument() {
 		String film = "101 Dalmatians";
 		Parser.getFilmTitle(film);
 	}
 	
-	// GRAFO getFilmTitle
 	// Camino [1, 2, 4, 6, 8]
 	@Test 
 	public void testHappyPathGetFilm() {
@@ -30,6 +30,20 @@ public class ParserTest {
 	}
 	
 	// GRAFO getFilmYear
+	// Camino [1, 2, 3]
+	@Test (expected=NullPointerException.class)
+	public void testNullYear() {
+		String film = null;
+		Parser.getFilmYear(film);
+	}
+
+	// Camino [1, 2, 4, 5]
+	@Test (expected=IllegalArgumentException.class)
+	public void testYearIlegalArgument() {
+		String film = "101 Dalmatians";
+		Parser.getFilmYear(film);
+	}
+	
 	// Camino [1, 2, 4, 6, 8]
 	@Test 
 	public void testHappyPathGetYear() {
@@ -38,6 +52,20 @@ public class ParserTest {
 	}
 	
 	// GRAFO getActorName
+	// Camino [1, 2, 3]
+	@Test (expected=NullPointerException.class)
+	public void testNullActorName() {
+		String film = null;
+		Parser.getActorName(film);
+	}
+	
+	// Camino [1, 2, 4, 5]
+	@Test (expected=IllegalArgumentException.class)
+	public void testActorNameIlegalArgument() {
+		String actor = "Braid";
+		Parser.getActorName(actor);
+	}
+	
 	// Camino [1, 2, 4, 6, 8]
 	@Test 
 	public void testHappyPathGetActorName() {
@@ -46,6 +74,20 @@ public class ParserTest {
 	}
 	
 	// GRAFO getActorSurname
+	// Camino [1, 2, 3]
+	@Test (expected=NullPointerException.class)
+	public void testNullActorSurname() {
+		String film = null;
+		Parser.getActorSurname(film);
+	}
+	
+	// Camino [1, 2, 4, 5]
+		@Test (expected=IllegalArgumentException.class)
+		public void testActorSurnameIlegalArgument() {
+			String actor = "Braid";
+			Parser.getActorSurname(actor);
+		}
+	
 	// Camino [1, 2, 4, 6, 8]
 	@Test 
 	public void testHappyPathGetActorSurname() {
