f9b26fbafb79d7f7a2f85395418644a1c6dbfdd5
cgomezcasa <cayegomez1995@gmail.com>
2018-01-24 16:51:34 +0100

    añadido test 4 grafo parserTest

--- src/test/java/urjc/isi/practicaFinal/ParserTest.java
+++ src/test/java/urjc/isi/practicaFinal/ParserTest.java
@@ -5,26 +5,35 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ParserTest {
-
+	
 	
 	// Camino [1, 2, 3]
 	@Test (expected=NullPointerException.class)
-	public void testNullFilm() {
+	public void testNullParam() {
 		String film = null;
 		Parser.getFilmTitle(film);
 	}
 	
 	// Camino [1, 2, 4, 5]
 	@Test (expected=IllegalArgumentException.class)
-	public void testFilmIlegalArgument() {
+	public void testParamIlegalArgument() {
 		String film = "101 Dalmatians";
 		Parser.getFilmTitle(film);
 	}
 	
+	// GRAFO getFilmTitle
 	// Camino [1, 2, 4, 6, 8]
 	@Test 
 	public void testHappyPathGetFilm() {
 		String film = "101 Dalmatians (2005)";
 		assertEquals(Parser.getFilmTitle(film), "101 Dalmatians");
+	}
+	
+	// GRAFO getFilmYear
+	// Camino [1, 2, 4, 6, 8]
+	@Test 
+	public void testHappyPathGetYear() {
+		String film = "101 Dalmatians (2005)";
+		assertEquals(Parser.getFilmYear(film), 2005);
 	}
 }
